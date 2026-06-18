513636998f13d5fd9dafbdb74cf5cbc36e0c2bf3
ignacio salazar <ignaciosalazar986@gmail.com>
2026-06-18 20:20:41 -0300

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,101 +2,24 @@ package org.example;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import org.example.DTOs.UsuarioDTO;
-import org.example.enums.Estado;
-import org.example.enums.FormaPago;
-import org.example.enums.Rol;
+
 import org.example.model.*;
 import org.example.repository.CategoriaRepository;
 import org.example.repository.ProductoRepository;
 import org.example.util.JPAUtil;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
-
-import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) {
+        ProductoRepository repositorioProducto = new ProductoRepository();
         CategoriaRepository repositorioCategoria = new CategoriaRepository();
 
-        ProductoRepository repositorioProducto = new ProductoRepository();
-/*Implementar en la clase Main un submenu de Categorias con las siguientes opciones:
-6. Alta: solicitar nombre y descripcion, crear la instancia, persistir y mostrar el ID generado.*/
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese nombre de categoría: ");
-        String nombreCategoria = scanner.nextLine();
-        System.out.print("Ingrese descripcion de categoría: ");
-        String descripcionCategoria = scanner.nextLine();
-        Categoria categoria1 = Categoria.builder()
-                .nombre(nombreCategoria)
-                .descripcion(descripcionCategoria)
-                .build();
-        repositorioCategoria.guardar(categoria1);
-        List<Categoria> categorias = repositorioCategoria.listarActivos();
-        Categoria ultimaCategoria = categorias.get(categorias.size() - 1);
-        categoria1.setId(ultimaCategoria.getId());
-        System.out.println("Categoria creada con ID: " + categoria1.getId());
-        /*7. Baja logica: solicitar el ID, marcar eliminado = true. Si no existe, mostrar mensaje de error.*/
-        for (Categoria cat : categorias) {
-            System.out.println("ID: " + cat.getId());
-            System.out.println("Nombre: " + cat.getNombre());
-        }
-        System.out.print("Ingrese el ID que desea eliminar: ");
-        Long eliminarID = Long.parseLong(scanner.nextLine());
-        Optional<Categoria> catAEliminar = repositorioCategoria.buscarPorId(eliminarID);
+        handleMenu();
 
-        if (catAEliminar != null && !catAEliminar.get().isEliminado()) {
-            repositorioCategoria.eliminarLogico(eliminarID);
-            System.out.println("Categoria eliminada");
-        } else {
-            System.out.println("ERROR - No se encontro el ID");
-        }
-/*8. Modificacion: solicitar el ID, mostrar valores actuales, permitir editar nombre y/o descripcion,
-persistir.*/
-        for (Categoria cat : categorias) {
-            System.out.println("ID: " + cat.getId());
-            System.out.println("Nombre: " + cat.getNombre());
-        }
-        System.out.print("Ingrese ID de la categoria a modificar: ");
-        Long idAModificar = Long.parseLong(scanner.nextLine());
-        Optional<Categoria> categoriaEncontrada = repositorioCategoria.buscarPorId(idAModificar);
-        if (categoriaEncontrada.isPresent()) {
-            Categoria catAModificar = categoriaEncontrada.get();
-            System.out.println("Valores actuales");
-            System.out.println("Nombre: " + catAModificar.getNombre());
-            System.out.println("Descripcion: " + catAModificar.getDescripcion());
-            System.out.print("Ingrese Opcion a modificar: ");
-            System.out.print("1 para modificar Nombre ");
-            System.out.print("2 para modificar Descripcion ");
-            System.out.print("0 Descartar ");
-
-            int opcion = Integer.parseInt(scanner.nextLine());
-            switch (opcion) {
-                case 1:
-                    System.out.print("Ingrese nuevo nombre: ");
-                    String nuevoNombre = scanner.nextLine();
-                    catAModificar.setNombre(nuevoNombre);
-                    break;
-                case 2:
-                    System.out.print("Ingrese nueva descripcion: ");
-                    String nuevaDescripcion = scanner.nextLine();
-                    catAModificar.setDescripcion(nuevaDescripcion);
-                    break;
-                case 0:
-                    System.out.println("Descartado");
-                    break;
-            }
-            EntityManagerFactory emf = JPAUtil.getEmf();
-            EntityManager em = emf.createEntityManager();
-            em.getTransaction().begin();
-            em.merge(catAModificar);
-            em.getTransaction().commit();
-            em.close();
-        } else System.out.println("No se encontro ID");
         /*9. Listado: mostrar todas las categorias activas con ID, nombre y descripcion.*/
         System.out.println("################ IDs Categorias Activas ################");
         for (Categoria cat : repositorioCategoria.listarActivos()) {
@@ -203,6 +126,135 @@ persistir.*/
             System.out.println("Precio: " + prod.getPrecio());
             System.out.println("Stock: " + prod.getStock());
             System.out.println("Categoria: " + prod.getCategoria());
+        }
+    }
+
+    public static void handleCategoria() {
+        CategoriaRepository repositorioCategoria = new CategoriaRepository();
+
+        System.out.println("-- Gestion Categorias --");
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1 Crear Categoria");
+        System.out.println("2 Para eliminar una categoria");
+        System.out.println("3 Para modificar categoria");
+        System.out.println("4 Para ver los productos de una categoria");
+        System.out.println("5 Para modificar una categoria");
+        System.out.println("0 Para volver al menos principal");
+        int opcionCategoria = Integer.parseInt(scanner.nextLine());
+        switch (opcionCategoria) {
+            case 0:
+                handleMenu();
+            case 1:
+                System.out.print("Ingrese nombre de categoría: ");
+                String nombreCategoria = scanner.nextLine();
+                System.out.print("Ingrese descripcion de categoría: ");
+                String descripcionCategoria = scanner.nextLine();
+                Categoria categoria1 = Categoria.builder()
+                        .nombre(nombreCategoria)
+                        .descripcion(descripcionCategoria)
+                        .build();
+                repositorioCategoria.guardar(categoria1);
+                List<Categoria> categorias = repositorioCategoria.listarActivos();
+                Categoria ultimaCategoria = categorias.get(categorias.size() - 1);
+                categoria1.setId(ultimaCategoria.getId());
+                System.out.println("########### Categoria creada con ID: " + categoria1.getId() + " ###########");
+                break;
+            case 2:
+                System.out.print("Ingrese el ID que desea eliminar: ");
+                Long eliminarID = Long.parseLong(scanner.nextLine());
+                Optional<Categoria> catAEliminar = repositorioCategoria.buscarPorId(eliminarID);
+                if (catAEliminar.isPresent() && !catAEliminar.get().isEliminado()) {
+                    repositorioCategoria.eliminarLogico(eliminarID);
+                    System.out.println("Categoria eliminada");
+                } else {
+                    System.out.println("ERROR - No se encontro el ID");
+                }
+                break;
+            case 3:
+                for (Categoria cat : repositorioCategoria.listarActivos()) {
+                    System.out.println("ID: " + cat.getId());
+                    System.out.println("Nombre: " + cat.getNombre());
+                }
+                System.out.print("Ingrese ID de la categoria a modificar: ");
+                Long idAModificar = Long.parseLong(scanner.nextLine());
+                Optional<Categoria> categoriaEncontrada = repositorioCategoria.buscarPorId(idAModificar);
+                if (categoriaEncontrada.isPresent() && !categoriaEncontrada.get().isEliminado()) {
+                    Categoria catAModificar = categoriaEncontrada.get();
+                    System.out.println("Valores actuales");
+                    System.out.println("Nombre: " + catAModificar.getNombre());
+                    System.out.println("Descripcion: " + catAModificar.getDescripcion());
+                    System.out.println("Ingrese Opcion a modificar: ");
+                    System.out.println("1 para modificar Nombre ");
+                    System.out.println("2 para modificar Descripcion ");
+                    System.out.println("0 Descartar ");
+
+                    int opcion = Integer.parseInt(scanner.nextLine());
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Ingrese nuevo nombre: ");
+                            System.out.println("Ingrese nuevo nombre: ");
+                            String nuevoNombre = scanner.nextLine();
+                            catAModificar.setNombre(nuevoNombre);
+                            break;
+                        case 2:
+                            System.out.println("Ingrese nueva descripcion: ");
+                            String nuevaDescripcion = scanner.nextLine();
+                            catAModificar.setDescripcion(nuevaDescripcion);
+                            break;
+                        case 0:
+                            System.out.println("Descartado");
+                            handleCategoria();
+                            break;
+                    }
+                    EntityManagerFactory emf = JPAUtil.getEmf();
+                    EntityManager em = emf.createEntityManager();
+                    em.getTransaction().begin();
+                    em.merge(catAModificar);
+                    em.getTransaction().commit();
+                    em.close();
+                    System.out.println("Categoria modificada");
+                    handleCategoria();
+                } else {
+                    System.out.println("No se encontro ID");
+                    handleCategoria();
+                }
+        }
+    }
+
+    public static void handleProducto() {
+        System.out.println("-- Gestion Producto --");
+        System.out.println("");
+        ProductoRepository repositorioProducto = new ProductoRepository();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1 Crear Producto");
+        System.out.println("2 Para listar productos");
+        System.out.println("3 Para eliminar un producto");
+        System.out.println("4 Para ver categoria del producto");
+        System.out.println("5 Para modificar un producto");
+        System.out.println("0 Para volver al menos principal");
+        int opcionCategoria = Integer.parseInt(scanner.nextLine());
+        switch (opcionCategoria) {
+            case 0:
+                handleMenu();
+            case 1:
+        }
+    }
+
+    public static void handleMenu() {
+        System.out.println("-- Menu Principal --");
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Elija una opcion para comenzar");
+        System.out.println("1 Manejar Categorias");
+        System.out.println("2 Manejar Productos");
+        int opcion1 = scanner.nextInt();
+        switch (opcion1) {
+            case 1:
+                handleCategoria();
+            case 2:
+                handleProducto();
         }
     }
 }
